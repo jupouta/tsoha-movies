7edfa98f1ca9de6aea99a43a5a1dd4d48ed917e5
Julia <juliapoutanen@gmail.com>
2022-09-19 13:23:12 +0300

    add base things for db

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import environ, path
+
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+class Config:
+    SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_APP = environ.get("FLASK_APP")
+
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
